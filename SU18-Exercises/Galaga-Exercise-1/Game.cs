80aae03fa877da2d90bd5ed9ea215f96cfab6865
Rasmus <lwb290@alumni.ku.dk>
2018-02-28 22:30:17 +0100

    Assets

---
+++ SU18-Exercises/Galaga-Exercise-1/Game.cs
@@ -0,0 +1,30 @@
+﻿using DIKUArcade;
+
+namespace Galaga_Exercise_1
+{
+    public class Game
+    {
+        private Window win;
+
+        public Game()
+        {
+
+            win = new Window("Galaga", 500, AspectRatio.R1X1);
+
+        }
+
+
+
+        public void GameLoop()
+        {
+            while (win.IsRunning())
+            {
+                win.PollEvents();
+                win.Clear();
+            }
+
+
+            win.SwapBuffers();
+        }
+    }
+}
--- SU18-Exercises/Galaga-Exercise-1/Program.cs
+++ SU18-Exercises/Galaga-Exercise-1/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Galaga_Exercise_1
+﻿
+namespace Galaga_Exercise_1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Game game = new Game();
+            game.GameLoop();
         }
     }
 }
